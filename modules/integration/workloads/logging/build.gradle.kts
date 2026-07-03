5d0f0e82d6d16d8cc356c168d3ef2320a6e64929
Ingo Kegel <ingo.kegel@ej-technologies.com>
2026-07-03 10:56:08 +0200

    switched log4j 1.2.x test dependency to reload4j to avoid security alerts

--- modules/integration/workloads/logging/build.gradle.kts
+++ modules/integration/workloads/logging/build.gradle.kts
@@ -17,6 +17,6 @@ dependencies {
     compileOnly(project(":agent:bundle"))
     implementation("org.apache.logging.log4j:log4j-api:2.25.4")
     implementation("org.apache.logging.log4j:log4j-core:2.25.4")
-    implementation("log4j:log4j:1.2.17")
+    implementation("ch.qos.reload4j:reload4j:1.2.26") // CVE-patched drop-in fork of log4j 1.2.x
     implementation("ch.qos.logback:logback-classic:1.5.37")
 }
