3a5c8a6ef7bd4924a668357ec69435eac582a832
Ingo Kegel <ingo.kegel@ej-technologies.com>
2026-07-07 09:48:40 +0200

    do not highlight unused workload classes in IDEA

--- modules/integration/workloads/src/main/java/com/jvmguard/integration/tests/jvmguard/matched/classes/TestPolicyHelper.java
+++ modules/integration/workloads/src/main/java/com/jvmguard/integration/tests/jvmguard/matched/classes/TestPolicyHelper.java
@@ -2,20 +2,13 @@ package com.jvmguard.integration.tests.jvmguard.matched.classes;
 
 import com.jvmguard.integration.util.SleepHelper;
 
-import java.util.logging.Filter;
 import java.util.logging.Level;
-import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 public class TestPolicyHelper {
     private final static Logger LOGGER = Logger.getLogger("default");
     static {
-        LOGGER.setFilter(new Filter() {
-            @Override
-            public boolean isLoggable(LogRecord record) {
-                return false;
-            }
-        });
+        LOGGER.setFilter(record -> false);
     }
 
     public static void log(Level level) {
