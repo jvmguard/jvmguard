ee01bda454290dd79f13a535cec3123c5622a169
Ingo Kegel <ingo.kegel@ej-technologies.com>
2026-07-17 17:15:01 +0200

    Show unmerged policies in transaction trees

--- modules/ui/src/main/kotlin/dev/jvmguard/ui/views/data/transactions/TransactionMode.kt
+++ modules/ui/src/main/kotlin/dev/jvmguard/ui/views/data/transactions/TransactionMode.kt
@@ -14,11 +14,11 @@ enum class TransactionMode(
 ) {
     CALL_TREE("Call tree", TransactionDataType.TRANSACTION, "Transaction", hasTimeLines = true, cumulateBacktraces = false) {
         override fun fetch(connection: ServerConnection, cursor: TransactionCursor): TransactionTreeData =
-            connection.getCallTree(cursor, true)
+            connection.getCallTree(cursor, false)
     },
     HOT_SPOTS("Hot spots", TransactionDataType.TRANSACTION, "Hot spot", hasTimeLines = true, cumulateBacktraces = true) {
         override fun fetch(connection: ServerConnection, cursor: TransactionCursor): TransactionTreeData =
-            connection.getHotspots(cursor, true)
+            connection.getHotspots(cursor, false)
     },
     OVERDUE("Overdue", TransactionDataType.OVERDUE, "Hot spot", hasTimeLines = false, cumulateBacktraces = true) {
         override fun fetch(connection: ServerConnection, cursor: TransactionCursor): TransactionTreeData =
