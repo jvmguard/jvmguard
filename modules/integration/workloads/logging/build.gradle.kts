cf174f679e18d9902585c289823264fe82f7be5e
Ingo Kegel <ingo.kegel@ej-technologies.com>
2026-07-19 11:16:27 +0200

    Java 1.8 tests require logback-classic 1.3.x

--- modules/integration/workloads/logging/build.gradle.kts
+++ modules/integration/workloads/logging/build.gradle.kts
@@ -18,5 +18,5 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api:2.25.4")
     implementation("org.apache.logging.log4j:log4j-core:2.25.4")
     implementation("ch.qos.reload4j:reload4j:1.2.26") // CVE-patched drop-in fork of log4j 1.2.x
-    implementation("ch.qos.logback:logback-classic:1.5.37")
+    implementation("ch.qos.logback:logback-classic:1.3.16") // 1.3.x is the last Java 8 compatible line
 }
